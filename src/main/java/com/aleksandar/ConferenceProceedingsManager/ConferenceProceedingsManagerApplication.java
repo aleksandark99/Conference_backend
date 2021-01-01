36dd93e74886c381117d32d9e142c5be63041c09
Aleksandar Kotur <aleksa.kotur@gmail.com>
2021-01-01 14:22:55 +0100

    initial commit

---
+++ src/main/java/com/aleksandar/ConferenceProceedingsManager/ConferenceProceedingsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.aleksandar.ConferenceProceedingsManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceProceedingsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceProceedingsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aleksandar/ConferenceProceedingsManager/ConferenceProceedingsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleksandar.ConferenceProceedingsManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceProceedingsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
